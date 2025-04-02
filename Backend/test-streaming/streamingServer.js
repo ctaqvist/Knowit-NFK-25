35e649429d4b099f5628473be698ca90fdea1c02
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-02 13:27:44 +0200

    test for streaming

---
+++ Backend/test-streaming/streamingServer.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const WebSocket = require('ws');
+const { spawn } = require('child_process');
+
+
+
+const wss = new WebSocket.Server({ port: 9000 });
+const clients = new Set();
+
+wss.on('connection', (ws) => {
+  console.log('WebSocket client connected');
+  clients.add(ws);
+
+  ws.on('close', () => {
+    console.log('WebSocket client disconnected');
+    clients.delete(ws);
+  });
+});
+
+const ffmpeg = spawn('ffmpeg', [
+  '-fflags', 'nobuffer',
+  '-i', 'srt://0.0.0.0:1234?mode=listener',
+  '-c:v', 'copy',
+  '-an',
+  '-f', 'mpegts',
+  'pipe:1'
+]);
+
+ffmpeg.stdout.on('data', (chunk) => {
+  for (const client of clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(chunk);
+    }
+  }
+});
+
+ffmpeg.stderr.on('data', (data) => {
+  console.error(`[ffmpeg] ${data}`);
+});
+
+ffmpeg.on('close', (code) => {
+  console.log(`FFmpeg exited with code ${code}`);
+});
