1f725fee83f7ae19949ebbf6004b24c4e96f4a5f
George <George.saba2003@gmail.com>
2025-04-03 11:09:35 +0200

    New Library for Battery health

--- Embedded/Arduino/battery_health/battery_health.ino
+++ Embedded/Arduino/battery_health/battery_health.ino
@@ -1,11 +1,12 @@
-const float R1 = 20000.0;
-const float R2 = 10000.0;
-
+#include "battery.h"
+//Constants
+const float Vref = 5.0;
 const float  battery_limit = 8.0; //Innna man får en varning
 const int ledPin = 13; //D13 är för att sätta på och stänga av LED
 
 
-const float Vref = 5.0;
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
@@ -15,19 +16,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int rawADC = analogRead(A0);                        //Läser spänningen analogt vid delningspunkten
-  float voltage = (rawADC * Vref) / 1023.0;           //0-1023 (10 bitars)
-  float batteryVoltage = voltage * ((R1 + R2) / R2);  //Beräkningen
+
+  //Funktion
+  float batteryHeath = BatteryHealth(A0, Vref);
+  //kan tas  bort
+  int rawADC = analogRead(A0);
+  float voltage= (rawADC * Vref) / 1023.0; 
 
   Serial.print("ADC: ");
   Serial.print(rawADC);
   Serial.print(" | Delningsspänning: ");
   Serial.print(voltage, 2);  //för antal värdesiffror
   Serial.print(" V | Batterispänning: ");
-  Serial.print(batteryVoltage, 2);
+  Serial.print(batteryHeath, 2);
   Serial.println(" V");
 
-  if(batteryVoltage < battery_limit) {
+  if(batteryHeath < battery_limit) {
     digitalWrite(ledPin, HIGH); //Tänder RÖD LED
   } else {
     digitalWrite(ledPin, LOW); //Släcker RÖD LED
