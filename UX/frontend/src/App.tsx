e8c4617612127d22660d48ab67e63d75b2c67db3
Athina <athina.kantis@gmail.com>
2025-04-03 16:08:27 +0300

    Add Frontend User Authorization

--- UX/frontend/src/App.tsx
+++ UX/frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router'
+import { AuthProvider } from './context/AuthProvider'
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
