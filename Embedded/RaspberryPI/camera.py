e6e82b38a9f448897e4a8d87ab8227bbbd59b274
Rama Muharam <165095768+RamaTech03@users.noreply.github.com>
2025-04-03 12:18:40 +0200

    29/funktion för kamera bild (#5)
    * Added code for camera func in campera.py
    
    * fixed files
    
    * Moved files/
    
    * Changed according to the review
    
    ---------
    
    Co-authored-by: Carl Tåqvist <carl.taqvist@knowit.se>
    Co-authored-by: MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>

--- Embedded/RaspberryPI/camera.py
+++ Embedded/RaspberryPI/camera.py
@@ -1,10 +1,14 @@
 
 import subprocess
-def take_picture():
-    command = "rpicam-still -t 2000 --width 1920 --height 1080 -o image-$(date +%Y-%m-%d).jpg"
 
+def take_picture(time = 2000,width=1920, height=1080):
+    
+
+    command = (
+        f'rpicam-still -t {time} --width {width} --height {height} '
+        f'-o image-$(date +%Y-%m-%d_%H).jpg'
+    )
     subprocess.run(command, shell=True)
-
 
 
 
