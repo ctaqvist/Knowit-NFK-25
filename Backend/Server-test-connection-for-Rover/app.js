acb46fa4794c3bd4193b4454d1d17bd33783a72a
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-07 10:48:28 +0200

    Use JSON.stringify() to ensure good json

--- Backend/Server-test-connection-for-Rover/app.js
+++ Backend/Server-test-connection-for-Rover/app.js
@@ -4,34 +4,44 @@ const wss = new WebSocket.Server({ port: 8080 });
 let clients = new Set();
 
 wss.on('connection', (ws) => {
-  clients.add(ws);
-  console.log('Client connected');
+    clients.add(ws);
+    console.log('Client connected');
 
-  ws.on('message', (msg) => {
-    console.log(`Received: ${msg}`);
+    ws.on('message', (msg) => {
+        console.log(`Received: ${msg}`);
 
-    // Special case: Ping/Pong
-    if (msg == "ping") {
-        ws.send(`{"response": "pong"}`);
-        return;
-    }
-
-    // Forward to all other clients
-    clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        if (client === ws) {
-          client.send(`{"response": "[Server echoes back]: ${msg}"}`)
-        } else {
-          client.send(`{"response": "[From a client]: ${msg}"}`);
+        // Special case: Ping/Pong
+        if (msg == "ping") {
+            ws.send(`{"response": "pong"}`);
+            return;
         }
-      }
-    });
-  });
 
-  ws.on('close', () => {
-    clients.delete(ws);
-    console.log('Client disconnected');
-  });
+        // Forward to all other clients
+        clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                if (client === ws) {
+                    client.send(
+                        JSON.stringify({
+                                "sender": "[SERVER]",
+                                "response": msg
+                            })
+                    )
+                } else {
+                    client.send(
+                        JSON.stringify({
+                            "sender": "[CLIENT]",
+                            "response": msg
+                        })
+                    );
+                }
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        clients.delete(ws);
+        console.log('Client disconnected');
+    });
 });
 
 console.log('WebSocket server running on ws://localhost:8080');
