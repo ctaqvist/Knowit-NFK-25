9a11ee2760b0e5e91f68f2c0f749e8a70982ae0a
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-07 15:51:00 +0200

    Install ws and add app.js

--- Backend/Server/app.js
+++ Backend/Server/app.js
@@ -1,5 +1,7 @@
 import SetUpRestAPI from "./router.js";
+import startWebSocketServer from "./websocketServer.js";
 
 // This is the root file
 const PORT = 8080;
 SetUpRestAPI(PORT);
+startWebSocketServer(PORT);
