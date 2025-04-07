ca82197c63faa7b54bd1a2bf744391083ce09e67
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-07 17:02:38 +0200

    create a dir if not existed

--- Backend/Server/saveImageFromRawData.js
+++ Backend/Server/saveImageFromRawData.js
@@ -2,5 +2,8 @@ import fs from 'fs';
 
 export default function SaveImage(data) {
     const buffer = Buffer.from(data, 'base64');
-    fs.writeFileSync('./img/image.jpg', buffer);
+    const outputPath = './img/image.jpg';
+    fs.mkdirSync('./img', { recursive: true }); // Ensure the directory exists
+    fs.writeFileSync(outputPath, buffer);
+    console.log(`Image saved to ${outputPath}`);
 };
