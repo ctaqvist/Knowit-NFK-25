048c2872e61ba84236d78b48964120bf1ce2c187
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-07 16:54:43 +0200

    Simple mainactivity

--- Application/app/src/main/java/se/emilkronholm/terrax9/MainActivity.kt
+++ Application/app/src/main/java/se/emilkronholm/terrax9/MainActivity.kt
@@ -16,8 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import se.emilkronholm.terrax9.ui.theme.Terrax9Theme
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,20 +41,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
 
+    val appViewModel: AppViewModel = viewModel()
+    appViewModel.connect()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Hello, world!")
+        Text("Welcome to TerraX9 app version 1!", fontSize = 35.sp, modifier = Modifier.padding(36.dp))
         Button(onClick = {
             println("Take picture")
+            appViewModel.sendPictureCommand()
         }) {
-            Text("Take picture!")
+            Text("Take picture!", fontSize = 25.sp, modifier = Modifier.padding(8.dp))
         }
     }
-}
-
-class AppViewModel: ViewModel() {
-    var socket: 
 }
