9a775fca15a7d632d5397e7620158455610efca3
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-08 11:42:33 +0200

    images can be saves with a format

--- Backend/Server/saveImageFromRawData.js
+++ Backend/Server/saveImageFromRawData.js
@@ -1,8 +1,8 @@
 import fs from 'fs';
 
-export default function SaveImage(data) {
+export default function SaveImage(data, format = `image_${new Date().toISOString().replace(/[:.]/g, '-')}`) {
     const buffer = Buffer.from(data, 'base64');
-    const outputPath = './img/image.jpg';
+    const outputPath = `./img/${format}.jpg`;
     fs.mkdirSync('./img', { recursive: true }); // Ensure the directory exists
     fs.writeFileSync(outputPath, buffer);
     console.log(`Image saved to ${outputPath}`);
