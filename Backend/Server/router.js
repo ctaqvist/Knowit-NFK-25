d65e8f9f0efc8cfc0bf49957200f4c396e45f85b
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-08 11:43:00 +0200

    the image directory is accessible for client

--- Backend/Server/router.js
+++ Backend/Server/router.js
@@ -1,9 +1,14 @@
 import express from 'express'
+import path from 'path'
+
 
 const app = express()
+
+app.use('/img', express.static(path.join(__dirname, "img"))) // access the images 
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Hello, world!" })
 })
+
 
 export default app
