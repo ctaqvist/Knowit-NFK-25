b01cfceed600f12c5aeaf16bf90c899c60a40ec1
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-09 14:55:54 +0200

    Added new route for getting an image

--- Backend/Server/router.js
+++ Backend/Server/router.js
@@ -8,23 +8,37 @@ const __dirname = path.dirname(__filename);
 
 const expressApp = express()
 
-const imageStaticPath = path.join(__dirname, "img")
-const baseURL = 'http://test.lazyloops.se/img/'
-expressApp.use('/img', express.static(imageStaticPath)) // access the images 
+const imagesStaticPath = path.join(__dirname, "img")
+const baseURL = 'http://test.lazyloops.se/images/'
+//expressApp.use('/img', express.static(imagesStaticPath)) // access the images 
 
 expressApp.get('/images', (req, res) => {
-    fs.readdir(imageStaticPath, (err, files) => { //fetch a list of images
+    // Fetch a list of images
+    fs.readdir(imagesStaticPath, (err, files) => { 
         if(err){
             console.error(err);
             res.status(500).send("error while getting images")
         }
-        const urls = files.map(imgName => { //create url of each image
-            return new URL(imgName, baseURL)
+        // Create url of each image
+        const urls = files.map(imgName => { 
+            return new URL(imgName, baseURL);
         })
-        res.send(urls)
+        res.send(urls);
     })
 })
 
+expressApp.get('/images/:imageName', (req, res) => {
+    const { imageName } = req.params
+    const filePath = path.join(imagesStaticPath, imageName);
+    // Check if the file path exist
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error("File not found:", imageName);
+            return res.status(404).send("Image not found");
+        }
+        res.sendFile(filePath);
+    });
+})
 expressApp.get('/', (req, res) => {
     res.status(200).json({ message: "Hello, world!" })
 })
