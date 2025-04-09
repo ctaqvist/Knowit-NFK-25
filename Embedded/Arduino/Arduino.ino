dc17daad860573325a289aca92f41c07b857af87
George Saba <126769262+George3235@users.noreply.github.com>
2025-04-09 12:07:44 +0200

    33/spot light funktion (#25)
    * Kopplingen med NPN funkar
    
    * Skapade funktionerna och de funkar
    
    * Delade upp koden

--- Embedded/Arduino/Arduino.ino
+++ Embedded/Arduino/Arduino.ino
@@ -1,12 +1,15 @@
 #include "battery_health_function.h"
+#include "spotLight_functions.h"
 //Constants
-const int Buzzer_Pin = 13; //D13 är för att sätta på och stänga av LED
+const int Buzzer_Pin = 13; //Styr Buzzern som tillhör Battery Protection system
+const int SpotLights_Pin = 12; //Sätter på och av spotlights
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(SpotLights_Pin, OUTPUT);
   pinMode(Buzzer_Pin, OUTPUT);
   Serial.begin(115200);
   delay(1000);
@@ -15,12 +18,12 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  
+  Spotlight_On();
+  delay(1000);
+  Spotlight_Off();
   checkBatteryAndWarn ();
   delay(1000);
   
  
   
 }
-
- 
---
+++ Embedded/Arduino/spotLight_functions.h
@@ -0,0 +1,7 @@
+#ifndef SPOTLIGHT_H
+#define SPOTLIGHT_H
+
+void Spotlight_On ();
+void Spotlight_Off ();
+
+#endif
