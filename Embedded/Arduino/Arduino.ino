b8343c531d4cd3d68fb339d025bfbccaa888e865
George <George.saba2003@gmail.com>
2025-04-09 11:31:54 +0200

    Kopplingen med NPN funkar

--- Embedded/Arduino/Arduino.ino
+++ Embedded/Arduino/Arduino.ino
@@ -1,12 +1,14 @@
 #include "battery_health_function.h"
 //Constants
-const int Buzzer_Pin = 13; //D13 är för att sätta på och stänga av LED
+const int Buzzer_Pin = 13; //Styr Buzzern som tillhör Battery Protection system
+const int SpotLights_Pin = 12; //Sätter på och av spotlights
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(SpotLights_Pin, OUTPUT);
   pinMode(Buzzer_Pin, OUTPUT);
   Serial.begin(115200);
   delay(1000);
@@ -15,12 +17,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  
+  digitalWrite(SpotLights_Pin, HIGH);
   checkBatteryAndWarn ();
   delay(1000);
   
  
   
 }
+
 
  
