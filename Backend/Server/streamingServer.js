44b28751ee13921066165038470fa433ee2ef18f
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-09 10:26:25 +0200

    Formatting

--- Backend/Server/streamingServer.js
+++ Backend/Server/streamingServer.js
@@ -3,45 +3,45 @@ import { WebSocket } from 'ws';
 import { WebSocketServer } from 'ws';
 
 function startStreamingServer(ports) {
-  const wss = new WebSocketServer({ port: ports.outputPort });
-  const clients = new Set();
+    const wss = new WebSocketServer({ port: ports.outputPort });
+    const clients = new Set();
 
-  // Output
-  wss.on('connection', (ws) => {
-    console.log('WebSocket client connected');
-    clients.add(ws);
+    // Output
+    wss.on('connection', (ws) => {
+        console.log('WebSocket client connected');
+        clients.add(ws);
 
-    ws.on('close', () => {
-      console.log('WebSocket client disconnected');
-      clients.delete(ws);
+        ws.on('close', () => {
+            console.log('WebSocket client disconnected');
+            clients.delete(ws);
+        });
     });
-  });
 
-  // Input
-  const stream = spawn('ffmpeg', [
-    '-fflags', 'nobuffer', // reduce buffering for lower latency
-    '-i', `srt://0.0.0.0:${ports.inputPort}?mode=listener`, // listen for incoming SRT connections on the given port
-    '-c:v', 'copy', // copy the video stream without re-encoding
-    '-an', // disable audio processing
-    '-f', 'mpegts', // output format is MPEG-TS
-    'pipe:1' // send the output to stdout
-  ])
+    // Input
+    const stream = spawn('ffmpeg', [
+        '-fflags', 'nobuffer', // reduce buffering for lower latency
+        '-i', `srt://0.0.0.0:${ports.inputPort}?mode=listener`, // listen for incoming SRT connections on the given port
+        '-c:v', 'copy', // copy the video stream without re-encoding
+        '-an', // disable audio processing
+        '-f', 'mpegts', // output format is MPEG-TS
+        'pipe:1' // send the output to stdout
+    ])
 
-  stream.stdout.on('data', (chunk) => {
-    for (const client of clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(chunk);
-      }
-    }
-  })
+    stream.stdout.on('data', (chunk) => {
+        for (const client of clients) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(chunk);
+            }
+        }
+    })
 
-  stream.stderr.on('data', (data) => {
-    console.error(`[ffmpeg] ${data}`);
-  });
+    stream.stderr.on('data', (data) => {
+        console.error(`[ffmpeg] ${data}`);
+    });
 
-  stream.on('close', (code) => {
-    console.log(`FFmpeg exited with code ${code}`);
-  });
+    stream.on('close', (code) => {
+        console.log(`FFmpeg exited with code ${code}`);
+    });
 }
 
 export default startStreamingServer
