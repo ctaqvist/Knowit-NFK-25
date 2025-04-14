c90b446a236d57102e13c4da3a909b6fb4c3c49a
George <George.saba2003@gmail.com>
2025-04-14 11:37:47 +0200

    löste en merge conflict

--- Embedded/Arduino/Arduino.ino
+++ Embedded/Arduino/Arduino.ino
@@ -1,95 +1,64 @@
 #include "battery_health_function.h"
+#include "spotLight_functions.h"
+
 //Constants
-const int Buzzer_Pin = 13; //D13 är för att sätta på och stänga av LED
+const int Buzzer_Pin = 13; //Styr Buzzern som tillhör Battery Protection system
 const int Motor_A = 2; //D2
 const int Motor_B = 4; //D4
 const int Motor_C = 7; //D7
 const int Motor_D = 8; //D8
 const int PWM_AB = 3; //D3
 const int PWM_CD = 5; //D5 
-
+const int SpotLights_Pin = 12; // Sätter på och av spotlights
 
 void forward () {
-  // Sätt riktning framåt för båda sidor
   digitalWrite(Motor_A, HIGH);
   digitalWrite(Motor_B, LOW);
   digitalWrite(Motor_C, HIGH);
   digitalWrite(Motor_D, LOW);
-
-  // Sätt PWM till motorerna (valfritt värde, ex. 200 av max 255)
   analogWrite(PWM_AB, 150);
   analogWrite(PWM_CD, 150);
-
 }
+
 void backward () {
-  // Sätt riktning framåt för båda sidor
   digitalWrite(Motor_A, LOW);
   digitalWrite(Motor_B, HIGH);
   digitalWrite(Motor_C, LOW);
   digitalWrite(Motor_D, HIGH);
-
-  // Sätt PWM till motorerna (valfritt värde, ex. 200 av max 255)
   analogWrite(PWM_AB, 150);
   analogWrite(PWM_CD, 150);
-
 }
-/* 
- * Funktionen för tankturn, allting ska vara hårdkodat här då det inte behövs något hastighet
-*/
+
 void tankTurn () {
-  // Sätt riktning framåt för båda sidor
-  // Grupp 1
   digitalWrite(Motor_A, HIGH);
   digitalWrite(Motor_B, LOW);
-  // Grunpp 2
   digitalWrite(Motor_C, LOW);
   digitalWrite(Motor_D, HIGH);
-
-  // Sätt PWM till motorerna (valfritt värde, ex. 200 av max 255)
   analogWrite(PWM_AB, 255);
   analogWrite(PWM_CD, 255);
-
 }
-// Funktion för att stoppa alla motorerna 
+
 void stopMotors() {
   analogWrite(PWM_AB, 0);
   analogWrite(PWM_CD, 0);
 }
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(Buzzer_Pin, OUTPUT);
 
+void setup() {
+  pinMode(SpotLights_Pin, OUTPUT);
+  pinMode(Buzzer_Pin, OUTPUT);
   pinMode(Motor_A, OUTPUT);
   pinMode(Motor_B, OUTPUT);
   pinMode(Motor_C, OUTPUT);
   pinMode(Motor_D, OUTPUT);
   pinMode(PWM_AB, OUTPUT);
   pinMode(PWM_CD, OUTPUT);
-
   Serial.begin(115200);
   delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Koden som används för att testa kommunikationen med Servern
-  /*if(Serial.available()) {
-    String command = Serial.readStringUntil('\n');
-    command.trim(); //för att ta bort alla mellanslag 
-
-    if(command == "fwd") {
-      forward();
-    } else if (command == "stop") {
-      stopMotors();
-    } else if (command == "tt") {
-      //tank turn 
-      tankTurn();
-    }
-  }*/
-
-  // Koden nedan används för att hårdkoda allting 
+  // Testkör motorer
   forward();
-  //checkBatteryAndWarn ();
   delay(3000);
   stopMotors();
   delay(2000);
@@ -102,7 +71,13 @@ void loop() {
   tankTurn();
   delay(1500);
   stopMotors();
-  while(true); //för att göra att allting endast körs en gång   
-}
 
- 
+  // Slå på och av spotlight, kontrollera batteri
+  Spotlight_On();
+  delay(1000);
+  Spotlight_Off();
+  checkBatteryAndWarn ();
+  delay(1000);
+
+  while(true); // Kör bara en gång
+}
