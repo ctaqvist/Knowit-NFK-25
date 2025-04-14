d05b9718d9933bd9457ee26b4ca2b19830f0504c
Athina <athina.kantis@gmail.com>
2025-04-14 14:56:22 +0300

    Setup Initial Admin Routes

---
+++ UX/backend/src/services/admin.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { SupabaseService } from './supabase.service';
+import { ApiResponse, Page } from 'src/types/types';
+
+/*
+// This service is for admins to use,
+// Update content, reviews etc.
+*/
+@Injectable()
+export class AdminService {
+  constructor(private readonly supabaseService: SupabaseService) {}
+
+
+  async updatePage(
+    page: string,
+    updatedPage: Page,
+  ): Promise<ApiResponse<string>> {
+    try {
+      const CLIENT = this.supabaseService.supabase
+      const {data, error} = await CLIENT.storage
+      .from('pages')
+      .upload(`${page}.json`, JSON.stringify(updatedPage), {
+        upsert: true
+      })
+      
+      if (error) {
+        console.error(error)
+        throw new Error('Error when updating data: ', error);
+      }
+
+      return {
+        message: 'File was successfully uploaded!',
+        data: data.path,
+        error: null
+      };
+
+    } catch (error) {
+      throw new Error(`Error when updating page: `, error);
+    }
+  }
+}
--- UX/frontend/src/context/ContentContext.tsx
+++ UX/frontend/src/context/ContentContext.tsx
@@ -1,9 +1,9 @@
 import { createContext } from 'react';
-import { FormattedPageData, FormattedReview } from '../types/types';
+import { Review, Pages } from '@/types/types';
 
 export interface ContentContextType {
-  pages: FormattedPageData | null,
-  reviews: FormattedReview[] | null
+  pages: Pages | null,
+  reviews: Review[] | null
 }
 
 export const ContentContext = createContext<ContentContextType | undefined>(undefined);
