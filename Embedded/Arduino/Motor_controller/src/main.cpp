f47302e59c8b24fd795d1a454581ba59f6a84679
George <George.saba2003@gmail.com>
2025-04-15 16:55:42 +0200

    la till consts pinnarna för komponenterna

--- Embedded/Arduino/Motor_controller/src/main.cpp
+++ Embedded/Arduino/Motor_controller/src/main.cpp
@@ -2,6 +2,13 @@
 //#include "CommandHandler.h"
 #include "Arduino.h"
 const int SpotLights_Pin = 12; // Pin for spotlights
+const int Buzzer_Pin = 13; //Styr Buzzern som tillhör Battery Protection system
+const int Motor_A = 2; //D2
+const int Motor_B = 4; //D4
+const int Motor_C = 7; //D7
+const int Motor_D = 8; //D8
+const int PWM_AB = 3; //D3
+const int PWM_CD = 5; //D5 
 
 void setup() {
   Serial.begin(115200);
