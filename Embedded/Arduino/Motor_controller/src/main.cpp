500acee313a323c8faeba3c9351d9a282a154986
George <George.saba2003@gmail.com>
2025-04-15 16:48:09 +0200

    La till env för uno R3

--- Embedded/Arduino/Motor_controller/src/main.cpp
+++ Embedded/Arduino/Motor_controller/src/main.cpp
@@ -1,14 +1,21 @@
-#include "MotorControl.h"
-#include "CommandHandler.h"
+//#include "MotorControl.h"
+//#include "CommandHandler.h"
+#include "Arduino.h"
+const int SpotLights_Pin = 12; // Pin for spotlights
 
 void setup() {
   Serial.begin(115200);
-  motorControl.init();
-  commandHandler.init();
+  //motorControl.init();
+  //commandHandler.init();
+  pinMode(SpotLights_Pin, OUTPUT);
 }
 
 void loop() {
-  commandHandler.listen();
-  motorControl.update();
+  //commandHandler.listen();
+  //motorControl.update();
+  digitalWrite(SpotLights_Pin, HIGH); // Turn on spotlights
+  delay(1000); // Wait for 1 second
+  digitalWrite(SpotLights_Pin, LOW); // Turn off spotlights
+  delay(1000); // Wait for 1 second
   delay(50);
 }
