286e9af693540e71d18f9e1c5cd5f98bf8f4f969
Rama Muharam <165095768+RamaTech03@users.noreply.github.com>
2025-04-15 09:02:00 +0200

    Added spot_lights commands (#43)
    * Added some comments for function to make them more clear
    
    * added command for spot_lights on/off
    
    ---------
    
    Co-authored-by: Joakim Möll <joakim.moll@outlook.com>

--- Embedded/RaspberryPI/commands/handler.py
+++ Embedded/RaspberryPI/commands/handler.py
@@ -2,6 +2,8 @@ from .picture import handle_pic_command
 from .stream import handle_stream_command, handle_stop_stream_command
 from .movement import handle_movement
 
+# This function processes incoming commands sent over a WebSocket connection.
+
 async def process_command(websocket, command, params, arduino):
     if command == "PIC":
         await handle_pic_command(websocket)
