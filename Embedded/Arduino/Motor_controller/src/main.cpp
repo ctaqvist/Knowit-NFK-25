20cdef99892d6aa9622155de9a6f9175a65ec6ce
George <George.saba2003@gmail.com>
2025-04-16 10:42:04 +0200

    Rensade gamla koden och tog bort den andra main filen så nu vi bara har en

--- Embedded/Arduino/Motor_controller/src/main.cpp
+++ Embedded/Arduino/Motor_controller/src/main.cpp
@@ -1,6 +1,7 @@
 //#include "MotorControl.h"
 //#include "CommandHandler.h"
 #include "Arduino.h"
+#include "Hardware/Battery/Battery_health.h"
 const int SpotLights_Pin = 12; // Pin for spotlights
 const int Buzzer_Pin = 13; //Styr Buzzern som tillhör Battery Protection system
 const int Motor_A = 2; //D2
@@ -10,19 +11,36 @@ const int Motor_D = 8; //D8
 const int PWM_AB = 3; //D3
 const int PWM_CD = 5; //D5 
 
+//Test funktion medan jag bygger upp rovern
+
+void forward() {
+  digitalWrite(Motor_A, HIGH);
+  digitalWrite(Motor_B, LOW);
+  digitalWrite(Motor_C, HIGH);
+  digitalWrite(Motor_D, LOW);
+  analogWrite(PWM_AB, 255);
+  analogWrite(PWM_CD, 255);
+}
+
+void stopMotors() {
+  analogWrite(PWM_AB, 0);
+  analogWrite(PWM_CD, 0);
+}
+
 void setup() {
   Serial.begin(115200);
   //motorControl.init();
   //commandHandler.init();
-  pinMode(SpotLights_Pin, OUTPUT);
 }
 
 void loop() {
   //commandHandler.listen();
   //motorControl.update();
-  digitalWrite(SpotLights_Pin, HIGH); // Turn on spotlights
-  delay(1000); // Wait for 1 second
-  digitalWrite(SpotLights_Pin, LOW); // Turn off spotlights
-  delay(1000); // Wait for 1 second
-  delay(50);
+  checkBatteryAndWarn ();
+  delay(1000);
+  forward();
+  delay(1000);
+  stopMotors();
+  delay(1000);
 }
+
