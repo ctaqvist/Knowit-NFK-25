9727e3671fdda94534d73b4f6801110aadcf7a09
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-16 13:23:20 +0200

    Imports

--- Application/app/src/main/java/se/emilkronholm/terrax9/ui/screens/controller/ControllerScreen.kt
+++ Application/app/src/main/java/se/emilkronholm/terrax9/ui/screens/controller/ControllerScreen.kt
@@ -1,13 +1,24 @@
 package se.emilkronholm.terrax9.ui.screens.controller
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.webkit.WebChromeClient
 import android.webkit.WebView
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -15,13 +26,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import se.emilkronholm.terrax9.ui.screens.test.VideoStream
+import se.emilkronholm.terrax9.R
 import se.emilkronholm.terrax9.ui.screens.controller.ViewModel
 import se.emilkronholm.terrax9.ui.theme.AppColors
 import kotlin.math.*
@@ -36,7 +50,7 @@ fun ControllerScreen() {
         Row(
             modifier = Modifier.fillMaxHeight(0.4f).aspectRatio(ratio = 16f/9f),
             verticalAlignment = Alignment.Bottom,
-            horizontalArrangement = Arrangement.End
+            horizontalArrangement = Arrangement.Center
         ) {
             VideoStream()
         }
