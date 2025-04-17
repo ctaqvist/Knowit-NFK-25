fa690ad7b89e15ded22e0d75d841eeaeb75fe85a
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-17 10:09:27 +0200

    Fix naming issue

--- Embedded/Arduino/Motor_controller/src/Hardware/SpotLights/spotlight.cpp
+++ Embedded/Arduino/Motor_controller/src/Hardware/SpotLights/spotlight.cpp
@@ -4,13 +4,13 @@
 const int SpotLights_Pin = 12;
 
 //Funktion som sätter på SpotLights
-void Spotlight_On ()
+void spotlightOn ()
 {
   digitalWrite(SpotLights_Pin, HIGH);
 }
 
 //Funktion som stänger av SpotLights
-void Spotlight_Off ()
+void spotlightOff ()
 {
   digitalWrite(SpotLights_Pin, LOW);
 }
