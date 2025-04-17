a42cfd689c1498252b1139265060eebfc295e68f
George <George.saba2003@gmail.com>
2025-04-17 09:49:26 +0200

    Ändrade på funktion namn

--- Embedded/Arduino/Motor_controller/src/Hardware/Battery/battery_health.h
+++ Embedded/Arduino/Motor_controller/src/Hardware/Battery/battery_health.h
@@ -1,13 +1,13 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
-//Funktion som läser av analoga värdet av batteriet för att ta reda på laddningen kvar
-// Vref- Reference voltage av kortet
-float BatteryHealth(int analogPin, float Vref);
-int checkBatteryLevel(float current_level) ;
-void checkBatteryAndWarn ();
-void warningSignal (bool level);
-void firstSignalStart ();
+// Funktion som läser av analoga värdet av batteriet för att ta reda på laddningen kvar
+//  Vref- Reference voltage av kortet
+float batteryHealth(int analogPin, float Vref);
+int checkBatteryLevel(float current_level);
+void checkBatteryAndWarn();
+void warningSignal(bool level);
+void firstSignalStart();
 void firstSignal();
 void lastSignalStart();
 void lastSignal();
