fbaeddb002e68e878ad30568445f17edcf7dff5f
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-22 16:06:23 +0200

    included the token in url

--- Backend/Server/jwtMiddleware.js
+++ Backend/Server/jwtMiddleware.js
@@ -3,8 +3,8 @@ import jwt from 'jsonwebtoken'
 function authCheck(SECRET_KEY){
     // The authCheck returns a middleware
     return (req, res, next) => {
-    // The token should be included to the header, Authorization : token
-    const token = req.header('Authorization')
+    // The token should be included as a query to the url
+    const token = req.query.token;
     if(!token) return res.status(401).json({ message : "Access denied!" });
     try {
         const decoded = jwt.verify(token, SECRET_KEY);
