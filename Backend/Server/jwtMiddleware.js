8e65e83099881de215e10db2595942ddd249a10a
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-22 14:30:24 +0200

    middleware class that checks the token

--- Backend/Server/jwtMiddleware.js
+++ Backend/Server/jwtMiddleware.js
@@ -1,11 +1,21 @@
 import jwt from 'jsonwebtoken'
-function authCheck(req, res, next){
-    const token = req.header('A')
-    const verification = jwt.verify(token, SECRET_KEY)
-    if(verification){
-        next
+
+function authCheck(SECRET_KEY){
+    // The authCheck returns a middleware
+    return (req, res, next) => {
+    // The token should be included to the header, Authorization : token
+    const token = req.header('Authorization')
+    if(!token) return res.status(401).json({ message : "Access denied!" });
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        // Add userId to the request
+        req.userId = token.userId;
+        next()
     }
-    else{
-        res
+    catch (error) {
+        res.status(401).json({message : "Invalid token!"})
     }
 }
+}
+
+export default authCheck
