4821ef9cf51907e0c74dfcd6fd0b3ad47d12480d
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-04-23 11:31:05 +0200

    updated the structure

--- Backend/Server/webSocketAuthMiddleware.js
+++ Backend/Server/webSocketAuthMiddleware.js
@@ -1,22 +1,12 @@
-import { WebSocketServer } from 'ws';
-import { WebSocket } from 'ws';
+
 import jwt from 'jsonwebtoken'
 
-function wsAuthCheck(ws, req){
+function wsAuthCheck(req, SECRET_KEY){
     // Create a url with connection request 
     const url = new URL(req.url, `http://${req.headers.host}`);
     // Get the token value in the request
     const token = url.searchParams.get('token');
-    try{
-        const decoded = jwt.verify(token, SECRET_KEY)
-        // Attach the token to the ws 
-        ws.token = decoded
-        console.log(`${decoded.username} is connected to web socket!`)
-    }
-    catch{
-        // Close the connection if token is invalid
-        ws.close(4001, 'Invalid token!')
-        return
-    }
+    if(!token) throw new Error('No token is provided!');
+    return jwt.verify(token, SECRET_KEY);
 }
 export default wsAuthCheck
