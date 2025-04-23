df5dc9480565430eb4e339ef4836e9733c7cce79
malinradstrom <rama23kb@student.ju.se>
2025-04-23 13:23:36 +0200

    implement commands steer-arm

--- Embedded/RaspberryPI/commands/arm/handler.py
+++ Embedded/RaspberryPI/commands/arm/handler.py
@@ -1,11 +1,13 @@
 from commands.arm.forwarder import forward_arm
 
-# Handles arm-specific commands only
-def is_arm_command(command):
-    return command == "arm"
-
+# Handles commands specific to the robot arm
 async def process_command(websocket, command, params):
-    if is_arm_command(command):
+    if "steer-arm" in params:
+       await forward_arm(params["steer-arm"],websocket)  
+       return
+# Fallback to direct command mode
+async def handle_arm_command(command, params, websocket):
+    if command == "arm":
         await forward_arm(params, websocket)
-    else:
-       await websocket.send("[ERROR] Unsupported arm command")
+    else:         
+        await websocket.send("[ERROR] Unsupported arm command")
