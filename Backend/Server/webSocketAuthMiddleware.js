5dc6fba795a856a2dbf791a2254e2898126db9f5
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-24 16:03:42 +0200

    Formatting

--- Backend/Server/webSocketAuthMiddleware.js
+++ Backend/Server/webSocketAuthMiddleware.js
@@ -4,12 +4,12 @@ import jwt from 'jsonwebtoken'
 const SECRET_KEY = "HiThisIsSecretKey";
 
 // This middleware checks whether the client is allowed to connect to web sockets on the server
-function wsAuthCheck(req){
+function wsAuthCheck(req) {
     // Create a url with connection request 
     const url = new URL(req.url, `http://${req.headers.host}`);
     // Get the token value in the request
     const token = url.searchParams.get('token');
-    if(!token) throw new Error('No token is provided!');
+    if (!token) throw new Error('No token is provided!');
     return jwt.verify(token, SECRET_KEY);
 }
 export default wsAuthCheck
