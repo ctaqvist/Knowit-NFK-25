259c0c200a1f7998fad28c965e69033b350a3735
George <George.saba2003@gmail.com>
2025-04-25 11:02:49 +0200

    bs3

--- Embedded/Arduino/Motor_controller/test/FakeMotorController/FakeMotorController.h
+++ Embedded/Arduino/Motor_controller/test/FakeMotorController/FakeMotorController.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include <iostream>
-
 class MotorController {
 public:
-    void DriveForward(int l, int r)  { std::cout << "Mock: DriveForward\n"; }
-    void DriveBackward(int l, int r) { std::cout << "Mock: DriveBackward\n"; }
-    void MakeTankTurnLeft(int s)     { std::cout << "Mock: TankLeft\n"; }
-    void MakeTankTurnRight(int s)    { std::cout << "Mock: TankRight\n"; }
-    void StopMotors()                { std::cout << "Mock: Stop\n"; }
+    void DriveForward(int l, int r);
+    void DriveBackward(int l, int r);
+    void MakeTankTurnLeft(int s);
+    void MakeTankTurnRight(int s);
+    void StopMotors();
 };
 
 extern MotorController motorController;
