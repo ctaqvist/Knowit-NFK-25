e4f6f878d872f6b3d853d41be3a525abb04dea38
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-25 10:24:53 +0200

    Update viewmodel

--- Application/app/src/main/java/se/emilkronholm/terrax9/ui/screens/controller/ViewModel.kt
+++ Application/app/src/main/java/se/emilkronholm/terrax9/ui/screens/controller/ViewModel.kt
@@ -1,17 +1,22 @@
 package se.emilkronholm.terrax9.ui.screens.controller
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import se.emilkronholm.terrax9.services.DataService
+import se.emilkronholm.terrax9.ui.screens.test.Commands
 
-class ViewModel(roverID: String): ViewModel() {
-
+class ViewModel(roverID: String) : ViewModel() {
     val dataService = DataService("terrax9.se")
     var job: Job? = null
-    fun onMovement(x: Float, y: Float) {
+    var isLighted by mutableStateOf<Boolean>(true)
 
+    fun onMovement(x: Float, y: Float) {
         // WARNING: Blocks UI
         dataService.ensureOpenConnection()
 
@@ -31,5 +36,17 @@ class ViewModel(roverID: String): ViewModel() {
     override fun onCleared() {
         super.onCleared()
         dataService.close()
+    }
+
+    // Helpers
+    fun takePhoto() = sendCommand(Commands.takePicture())
+    fun toggleLights() {
+        isLighted = !isLighted
+
+        if (isLighted) {
+            sendCommand(Commands.closeHeadlights())
+        } else {
+            sendCommand(Commands.startHeadlights())
+        }
     }
 }
