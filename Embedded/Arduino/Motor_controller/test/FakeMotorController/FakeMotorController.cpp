a335feb2574fd16f96a8c459edbc875801afe842
George <George.saba2003@gmail.com>
2025-04-25 15:58:44 +0200

    Revert "bs3"
    This reverts commit 259c0c200a1f7998fad28c965e69033b350a3735.
    
    Backar

--- Embedded/Arduino/Motor_controller/test/FakeMotorController/FakeMotorController.cpp
+++ Embedded/Arduino/Motor_controller/test/FakeMotorController/FakeMotorController.cpp
@@ -1,22 +1,3 @@
 #include "FakeMotorController.h"
-#include <iostream>
 
-// Global instans
 MotorController motorController;
-
-// Mock-metoder (du saknar dessa just nu)
-void MotorController::DriveForward(int l, int r) {
-    std::cout << "Mock: DriveForward\n";
-}
-void MotorController::DriveBackward(int l, int r) {
-    std::cout << "Mock: DriveBackward\n";
-}
-void MotorController::MakeTankTurnLeft(int s) {
-    std::cout << "Mock: TankLeft\n";
-}
-void MotorController::MakeTankTurnRight(int s) {
-    std::cout << "Mock: TankRight\n";
-}
-void MotorController::StopMotors() {
-    std::cout << "Mock: Stop\n";
-}
