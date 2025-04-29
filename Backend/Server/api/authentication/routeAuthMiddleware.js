bcf5a92a2b5e6a6c4dc135ea34e3ea7218ee106c
Emil Kronholm <emil.kronholm@gmail.com>
2025-04-29 13:15:36 +0200

    Restructure

--- Backend/Server/api/authentication/routeAuthMiddleware.js
+++ Backend/Server/api/authentication/routeAuthMiddleware.js
@@ -1,34 +1,31 @@
 import jwt from 'jsonwebtoken'
-
-const SECRET_KEY = "HiThisIsSecretKey";
+import dotenv from 'dotenv'
+dotenv.config();
+const SECRET_KEY = process.env.SECRET_KEY;
 
 // This middleware checks whether a user is authorized to access a route on the server.
-function authCheck() {
-    // The authCheck returns a middleware
-    return (req, res, next) => {
-        // The token should be included as a query to the url
-        const token = req.query.token;
+function authCheck(req, res, next) {
+    // The token should be included as a query to the url
+    const token = req.query.token;
         
-        // If no token exists, deny access
-        if (!token) {
-            // XXX: CHANGE DURING PRODUCTION
-            // During development, if no token is provided, allow access anyway
-            next();
-            return;
-            // return res.status(401).json({ message: "Access denied!" });
-        }
+    // If no token exists, deny access
+    if (!token) {
+        // XXX: CHANGE DURING PRODUCTION -- during development, if no token is provided, allow access anyway
+        next();
+        return;
+        // return res.status(401).json({ message: "Access denied!" });
+    }
 
-        try {
-            // Try to verify token
-            jwt.verify(token, SECRET_KEY);
+    try {
+        // Try to verify token
+        jwt.verify(token, SECRET_KEY);
 
-            // Add userId to the request
-            req.userId = token.userId;
-            next();
-        }
-        catch (error) {
-            res.status(401).json({ message: "Invalid token!" });
-        }
+        // Add userId to the request
+        req.userId = token.userId;
+        next();
+    }
+    catch (error) {
+        res.status(401).json({ message: "Invalid token!" });
     }
 }
 
