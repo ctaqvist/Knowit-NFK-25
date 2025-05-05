49517a9f4ae9ae63515ad9fa8270e9365fe09a05
Emil Kronholm <emil.kronholm@gmail.com>
2025-05-05 16:56:43 +0200

    WIP: Login screen

--- Application/app/src/main/java/se/terrax9/ui/screens/login/LoginScreen.kt
+++ Application/app/src/main/java/se/terrax9/ui/screens/login/LoginScreen.kt
@@ -1,15 +1,20 @@
 package se.terrax9.ui.screens.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material.TextFieldColors
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -21,17 +26,87 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import se.terrax9.R
 import se.terrax9.ui.theme.AppColors
 
 @Composable
 fun LoginScreen() {
-    Input(
-        tag = "E-mail",
-        "Enter your e-mail",
-        onChange = {}
-    )
+
+    val viewModel: LoginViewModel = viewModel()
+    var email = viewModel.email
+    var password = viewModel.password
+    var status = viewModel.status
+    var failed = viewModel.failed
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            painter = painterResource(R.drawable.background_details),
+            contentDescription = null
+        )
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+    ) {
+        Text("Log In", style = MaterialTheme.typography.headlineMedium)
+        Text("DEVELOPER INFO: $status")
+        Input(
+            tag = "E-mail",
+            placeholder = "Enter your e-mail",
+            onChange = { email = it; failed = false },
+            isError = failed
+        )
+
+        Input(
+            tag = "Password",
+            placeholder = "Enter password",
+            onChange = { password = it; failed = false; },
+            isPassword = true,
+            isError = failed
+        )
+
+        // Error message text
+        if (failed) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.error_icon),
+                    contentDescription = null
+                )
+                Text("E-mail or password is wrong", color = AppColors.Error, style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+
+        Button(
+            onClick = viewModel::login,
+            modifier = Modifier
+                .background(color = Color(0xFF5526FF), shape = RoundedCornerShape(size = 16.dp))
+                .padding(15.dp)
+        ) {
+            Text("LOG IN", style = MaterialTheme.typography.headlineSmall, fontSize = 18.sp)
+        }
+
+
+
+        Text("Don't have an account?")
+        Text("Sign Up")
+    }
 }
 
 @Composable
@@ -40,34 +115,39 @@ fun Input(
     placeholder: String,
     onChange: (String) -> Unit,
     modifier: Modifier = Modifier,
-    isPassword: Boolean = false
+    isPassword: Boolean = false,
+    isError: Boolean = true,
+    errorText: String = ""
 ) {
     var text by remember { mutableStateOf("") }
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(tag, color = Color.White)
+    var isShown by remember { mutableStateOf(false) }
+
+    Column {
+        Text(tag, color = Color.White, style = MaterialTheme.typography.headlineSmall)
         Spacer(modifier = Modifier.padding(horizontal = 8.dp))
         TextField(
             value = text,
             onValueChange = { text = it },
             modifier = modifier
-                .align(alignment = Alignment.CenterVertically)
                 .border(
-                    width = 1.dp,
-                    color = Color(0xFFBCC5FF),
+                    width = 2.dp,
+                    color = if (isError) AppColors.Error else AppColors.Primary,
                     shape = RoundedCornerShape(size = 10.dp)
                 )
-                .background(color = Color(0x1ABCC5FF), shape = RoundedCornerShape(size = 10.dp))
+                .background(
+                    color = Color(0x1ABCC5FF),
+                    shape = RoundedCornerShape(size = 10.dp)
+                )
                 .width(547.dp)
-                .height(106.dp),
+                .padding(vertical = 5.dp),
             colors = TextFieldDefaults.colors(
                 focusedContainerColor = Color.Transparent,
                 unfocusedContainerColor = Color.Transparent,
                 cursorColor = Color.White,
                 focusedTextColor = Color.White,
                 unfocusedTextColor = Color.White
-            )
+            ),
+            visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None
         )
     }
 }
