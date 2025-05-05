76627cfccf8cc35228ea893de4e7060ceb90de5c
Emil Kronholm <emil.kronholm@gmail.com>
2025-05-05 11:36:06 +0200

    Recreate as independent of android ViewModel

--- Application/app/src/main/java/se/terrax9/ui/screens/controller/JoyStickViewModel.kt
+++ Application/app/src/main/java/se/terrax9/ui/screens/controller/JoyStickViewModel.kt
@@ -1,17 +1,20 @@
 package se.terrax9.ui.screens.controller
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
-class JoyStickViewModel(val command: (Float, Float) -> String, val sendMessage: (String) -> Unit) : ViewModel() {
+class JoyStickViewModel(val command: (Float, Float) -> String, val sendMessage: (String) -> Unit) {
     private var lastSent: Pair<Float, Float> = Pair(0f, 0f)
 
-    fun onMove(new: Pair<Float, Float>) {
+    fun onMove(x: Float, y: Float) {
+        val new: Pair<Float, Float> = Pair(x, y)
         if (shouldBeSentAgain(new, lastSent)) {
-            sendMessage(command(new.first, new.second))
+            val command: String = command(new.first, new.second)
+            sendMessage(command)
             lastSent = new
         }
     }
@@ -23,6 +26,9 @@ class JoyStickViewModel(val command: (Float, Float) -> String, val sendMessage: 
         val newMinValue = (new.first.absoluteValue == 0f && old.first.absoluteValue != 0f)
                 || (new.second.absoluteValue == 0f && old.second.absoluteValue != 0f)
         val newExtremeValue = newMinValue || newMaxValue
-        return newExtremeValue
+
+        val newValue =
+            (lastSent.first - new.first).absoluteValue > 0.02 || ((lastSent.second - new.second).absoluteValue > 0.02)
+        return newValue || newExtremeValue
     }
 }
