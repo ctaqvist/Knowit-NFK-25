9404894374f8accdefa448d5077e89094fdbfaa4
Emil Kronholm <emil.kronholm@gmail.com>
2025-05-08 15:02:23 +0200

    Update protocol and implement sliders
    Update Slider.kt
    
    Formatting
    
    Create SliderRow.kt
    
    Create Slider.kt
    
    Update ControllerScreen.kt
    
    Update protocol for 3 sliders

--- Application/app/src/main/java/se/terrax9/ui/screens/controller/ControllerScreen.kt
+++ Application/app/src/main/java/se/terrax9/ui/screens/controller/ControllerScreen.kt
@@ -120,16 +120,11 @@ fun BottomDashboard(viewModel: ControllerViewModel) {
                 iconRes = R.drawable.camera,
                 onClick = viewModel::takePhoto
             )
-            JoyStick(
-                onMove = viewModel::sendCommand,
-                getCommand = Commands::arm,
-                isFixed = true
-            )
-            JoyStick(
-                onMove = viewModel::sendCommand,
-                getCommand = Commands::claw,
-                isFixed = true
-            )
+
+            SliderRow { x, y, z ->
+                viewModel.sendCommand(Commands.steerArm(x, y, z))
+            }
+
         }
     }
 }
---
+++ Application/app/src/main/java/se/terrax9/ui/screens/controller/SliderRow.kt
@@ -0,0 +1,81 @@
+package se.terrax9.ui.screens.controller
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
+import se.terrax9.services.Commands
+import se.terrax9.services.DataService
+import kotlin.math.absoluteValue
+
+@Composable
+fun SliderRow(onChange: (Float, Float, Float) -> Unit) {
+    val vm = SliderRowViewModel(onChange = { x, y, z ->
+        onChange(x, y, z)
+
+    })
+
+    Row() {
+        Slider(onMove = { vm.updateShoulder(it) })
+
+        Slider(onMove = { vm.updateElbow(it) })
+
+        Slider(onMove = { vm.updateClaw(it) })
+    }
+}
+
+class SliderRowViewModel(val onChange: (Float, Float, Float) -> Unit) {
+    var shoulder: Float = 0f
+    private var _oldShoulder: Float = shoulder
+
+    var elbow: Float = 0f
+    private var _oldElbow: Float = elbow
+
+    var claw: Float = 0f
+    private var _oldClaw: Float = claw
+
+    fun updateShoulder(it: Float) {
+        shoulder = it
+        _onChange()
+    }
+
+    fun updateElbow(it: Float) {
+        elbow = it
+        _onChange()
+    }
+
+    fun updateClaw(it: Float) {
+        claw = it
+        _onChange()
+    }
+
+    private fun _onChange() {
+        if (shouldBeSentAgain(shoulder, _oldShoulder) ||
+            shouldBeSentAgain(elbow, _oldElbow) ||
+            shouldBeSentAgain(claw, _oldClaw)) {
+            onChange(shoulder, elbow, claw)
+            _oldClaw = claw
+            _oldElbow = elbow
+            _oldShoulder = shoulder
+        }
+    }
+
+    private fun shouldBeSentAgain(new: Float, old: Float): Boolean {
+        val newMaxValue = new.absoluteValue == 1f && old.absoluteValue != 1f
+        val newMinValue = new.absoluteValue == 0f && old.absoluteValue != 0f
+        val newExtremeValue = newMinValue || newMaxValue
+
+        val newValue = (old - new).absoluteValue > 0.1
+        return newValue || newExtremeValue
+    }
+
+
+}
