e0a137144079cb506fd09d6705880e9605f1228d
Athina <athina.kantis@gmail.com>
2025-05-08 14:58:48 +0300

    Add Contact Page

---
+++ UX/frontend/src/components/CountrySelect.tsx
@@ -0,0 +1,180 @@
+import {
+    INITIAL_COUNTRIES,
+} from '@/utils/data/countries';
+import { Box, Button, MenuItem, Popover, TextField } from '@mui/material';
+import {
+    ChangeEvent,
+    SetStateAction,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
+import { ContactForm } from '@/types/types';
+import Icon from './Icon';
+
+type CountrySelectProps = {
+    formData: ContactForm;
+    setFormData: React.Dispatch<SetStateAction<ContactForm>>;
+};
+
+export default function CountrySelect({
+    setFormData,
+    formData,
+}: CountrySelectProps) {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [countries, setCountries] = useState(INITIAL_COUNTRIES);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const anchorRef = useRef(null)
+
+    const handleNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            telephone: {
+                ...formData.telephone,
+                number: e.target.value,
+            },
+        });
+    };
+
+    const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleSelectCountry = (e: React.MouseEvent<HTMLLIElement>) => {
+        document.querySelector('.selectedCountry')?.classList.remove('selectedCountry')
+        const target = e.currentTarget as HTMLLIElement;
+        const value = target.dataset.value;
+
+        target.classList.add('selectedCountry')
+        const country = countries.find(c => c.code === value)
+        if (!country) return;
+        setFormData({
+            ...formData,
+            telephone: {
+                ...country,
+                number: formData.telephone.number
+            }
+        })
+    };
+
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value)
+        const filteredCountries = INITIAL_COUNTRIES.filter(c => c.label.toLowerCase().includes(e.target.value))
+        setCountries(filteredCountries)
+    }
+
+    useEffect(() => {
+        if (open) {
+            setTimeout(() => {
+                const el = document.querySelector(`[data-value="${formData.telephone.code}"]`)
+                if (!el) return;
+                el.classList.add('selectedCountry')
+            }, 10)
+        }
+    }, [open])
+
+    return (
+        <Box sx={{ display: 'flex', }}
+            ref={anchorRef}>
+            <Button
+                aria-haspopup='true'
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleMenuOpen}
+
+                sx={{
+                    height: 61, borderRadius: 0, border: '1px solid rgba(188, 197, 255, 1)', borderTopLeftRadius: '10px', borderBottomLeftRadius: '10px', width: 96, gap: '10px', backgroundColor: 'rgba(188, 197, 255, 0.3)', borderRight: 0
+                }}
+            >
+                <img
+                    loading='lazy'
+                    width='33'
+                    srcSet={`https://flagcdn.com/w40/${formData?.telephone?.code?.toLowerCase()}.png 2x`}
+                    src={`https://flagcdn.com/w20/${formData?.telephone?.code?.toLowerCase()}.png`}
+                    alt=''
+                    style={{ borderRadius: 2 }}
+                />
+                <Icon
+                    src='/src/assets/Icon_arrow_down.svg'
+                    width='18px'
+                    height='14px'
+                />
+            </Button>
+            <TextField
+                variant='outlined'
+                disabled
+                value={`+${formData.telephone.phone}`}
+
+                sx={{
+                    width: '100%', maxWidth: 317,
+                    '& .MuiInputBase-root': { borderRadius: 0, paddingRight: 0, borderTopRightRadius: 10, borderBottomRightRadius: 10 },
+                    '& input': { paddingRight: 0, borderRight: 'none' },
+                    '& input.Mui-disabled': { color: 'white', opacity: 1, WebkitTextFillColor: 'white', }
+                }}
+            />
+            <TextField
+                value={formData.telephone.number}
+                onChange={handleNumberChange}
+                autoComplete='false'
+                inputProps={{
+                    autocomplete: 'new-password',
+                    form: {
+                        autocomplete: 'off',
+                    },
+                }}
+                sx={{
+                    flex: 1, position: 'absolute', right: 0, width: 317,
+                    backgroundColor: 'transparent',
+                    '& .MuiInputBase-root': { borderTopLeftRadius: 0, borderBottomLeftRadius: 0, borderLeft: 0, backgroundColor: 'transparent', paddingLeft: '40px' }
+                }}
+            />
+
+            {open && (
+                <Popover
+                    id='country-select-menu'
+                    onClose={handleMenuClose}
+                    anchorEl={anchorEl}
+                    open={open}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }}
+
+                >
+                    <MenuItem sx={{ position: 'relative' }}>
+                        <Box sx={{ position: 'absolute' }}>
+                            <Icon src='/src/assets/Icon_search.svg' />
+                        </Box>
+                        <TextField
+                            placeholder='Search for country'
+                            id='custom-search'
+                            variant='standard'
+                            value={searchTerm}
+                            onChange={handleSearch}
+                            slotProps={{ input: { disableUnderline: true } }}
+
+                        />
+                    </MenuItem>
+                    {countries.map(c => (
+                        <MenuItem key={c.code}
+                            data-value={c.code}
+                            onClick={handleSelectCountry}>
+                            <img
+                                loading='lazy'
+                                width='20'
+                                srcSet={`https://flagcdn.com/w40/${c.code.toLowerCase()}.png 2x`}
+                                src={`https://flagcdn.com/w20/${c.code.toLowerCase()}.png`}
+                                alt=''
+                                style={{ borderRadius: 2 }}
+                            />
+                            {`${c.label} (+${c.phone})`}
+                        </MenuItem>
+                    ))
+                    }
+                </Popover >
+            )
+            }
+        </Box >
+    );
+}
---
+++ UX/frontend/src/components/DateCalendar.tsx
@@ -0,0 +1,86 @@
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
+import { SetStateAction } from 'react';
+import { ContactForm } from '@/types/types';
+import dayjs, { Dayjs } from 'dayjs';
+
+type BasicDateCalendarProps = {
+  formData: ContactForm;
+  setFormData: React.Dispatch<SetStateAction<ContactForm>>
+}
+
+export default function BasicDateCalendar({ setFormData, formData }: BasicDateCalendarProps) {
+
+  const handleChange = (date: Dayjs | null) => {
+    const DAY_JS_DATE = dayjs(date)
+    if (!date) return;
+    setFormData({
+      ...formData,
+      booking: {
+        ...formData.booking!,
+        date: DAY_JS_DATE
+      },
+    });
+  };
+
+  function CustomDay(
+    props: PickersDayProps
+  ) {
+    const { day } = props;
+    const dayOfWeek = day.day(); // Sunday: 0, Saturday: 6
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      return <span style={{ display: 'none' }} />; // Hide weekends
+    }
+    return <PickersDay {...props} />;
+  }
+
+
+
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar
+        views={['day']}
+        onChange={handleChange}
+        shouldDisableDate={(date) => date.day() === 0 || date.day() === 6}
+        slots={{ day: CustomDay }}
+        disableHighlightToday
+        value={formData.booking?.date ? dayjs(formData.booking.date) : null}
+        sx={{
+          backgroundColor: 'rgba(188, 197, 255, 0.1)', border: '1px solid rgba(188, 197, 255, 1)', borderRadius: '10px', padding: '41px 40px', m: 0, gap: '30px', maxHeight: '100%', height: '100%', maxWidth: 370, width: '100%', position: 'relative',
+
+          // Month/Year select
+          '& .MuiPickersCalendarHeader-labelContainer': { margin: 0, position: 'absolute', left: '50%', transform: 'translateX(-50%)' },
+          '& .MuiPickersArrowSwitcher-root': { width: '100%', justifyContent: 'space-between' },
+
+          // Calendar header
+          '& .MuiPickersCalendarHeader-root': { margin: 0, p: 0, overflow: 'hidden' },
+          '& .MuiPickersCalendarHeader-root::-webkit-scrollbar': { display: 'none' },
+          '& .css-1r48rix-MuiButtonBase-root-MuiIconButton-root-MuiPickersCalendarHeader-switchViewButton': { display: 'none' },
+          '& path': { fill: 'rgba(85, 38, 255, 1)' },
+          '& .MuiPickersCalendarHeader-root button': { p: 0 },
+
+          // Weekday label
+          '& .MuiDayCalendar-header': { justifyContent: 'space-between', mb: '10px' },
+          '& .MuiDayCalendar-weekDayLabel': { color: 'rgba(85, 38, 255, 1)', fontWeight: 700, fontFamily: 'Instrument Sans, sans-serif', fontSize: 20 },
+          '& .MuiPickersDay-root': { fontFamily: 'Instrument Sans, sans-serif', fontSize: 18 },
+          '& .css-1ckygne-MuiButtonBase-root-MuiPickersDay-root.Mui-disabled:not(.Mui-selected)': { color: 'white', opacity: 0.3 },
+          '& .MuiDayCalendar-weekContainer': { justifyContent: 'space-between', mb: '10px' },
+
+          // Do not show Saturday + Sunday label
+          '& .MuiDayCalendar-header :is([aria-label="Saturday"], [aria-label="Sunday"])': { display: 'none' },
+
+          // Selected State
+          '& .Mui-selected': { backgroundColor: 'rgba(85, 38, 255, 1) !important' },
+
+
+          '& .MuiPickersSlideTransition-root': { height: 230, minHeight: 200, overflow: 'hidden' }
+
+        }}
+        disablePast
+      />
+    </LocalizationProvider>
+  );
+}
