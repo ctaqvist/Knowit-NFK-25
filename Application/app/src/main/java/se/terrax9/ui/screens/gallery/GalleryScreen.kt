3e64b9d31b8a6f213a677083dc38d235252d6c4b
MojiZZZ <124263501+MojiZZZ@users.noreply.github.com>
2025-05-09 11:12:37 +0200

    WIP

--- Application/app/src/main/java/se/terrax9/ui/screens/gallery/GalleryScreen.kt
+++ Application/app/src/main/java/se/terrax9/ui/screens/gallery/GalleryScreen.kt
@@ -1,11 +1,14 @@
 import android.util.Log
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -27,14 +30,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import se.terrax9.R
 import se.terrax9.ui.screens.Gallery.GalleryViewModel
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -94,34 +103,46 @@ fun GalleryScreen(viewModel: GalleryViewModel = viewModel()) {
                 val (others, sortedGroups) = imageGroups.partition { it.title == "Others" }
                 val sortedImageGroups = sortedGroups + others
 
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(5),
-                    state = gridState,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 10.dp),
-                    verticalArrangement = Arrangement.spacedBy(4.dp),
-                    horizontalArrangement = Arrangement.spacedBy(4.dp)
-                ) {
-                    sortedImageGroups.forEach { group ->
-                        // Full-width title
-                        item(span = { GridItemSpan(maxLineSpan) }) {
-                            Text(
-                                text = group.title,
-                                style = MaterialTheme.typography.titleMedium,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 8.dp)
-                            )
-                        }
-                        // Then all the images in that title-group
-                        items(group.images) { url ->
-                            HttpImage(
-                                url = url,
-                                modifier = Modifier
-                                    .aspectRatio(1f)
-                                    .fillMaxWidth()
-                            )
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(R.drawable.backgroundpicture),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                    )
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(5),
+                        state = gridState,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(top = 50.dp, bottom = 50.dp, start = 80.dp, end = 80.dp),
+                        verticalArrangement = Arrangement.spacedBy(19.dp),
+                        horizontalArrangement = Arrangement.spacedBy(19.dp),
+                        contentPadding = PaddingValues(0.dp)
+                    ) {
+                        sortedImageGroups.forEach { group ->
+                            // Full-width title
+                            item(span = { GridItemSpan(maxLineSpan) }) {
+                                Text(
+                                    text = group.title,
+                                    style = TextStyle(
+                                        fontSize = 32.sp,
+                                        fontFamily = FontFamily(Font(R.font.lexendera)),
+                                        fontWeight = FontWeight(500),
+                                        color = Color(0xFFFFFFFF),
+                                    ),
+                                    modifier = Modifier.padding(bottom = 20.dp, top = 10.dp) // Add padding to the bottom of the text
+                                )
+                            }
+                            // Then all the images in that title-group
+                            items(group.images) { url ->
+                                HttpImage(
+                                    url = url,
+                                    modifier = Modifier
+                                        .aspectRatio(1f)
+                                        .height(250.dp)
+                                        .width(150.dp)
+                                )
+                            }
                         }
                     }
                 }
