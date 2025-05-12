05baa7a6e7f75996e954d5198334de1402e11022
Athina Kantis <athina.kantis@gmail.com>
2025-05-12 12:05:45 +0300

    65/coding/programming booking/contact page (#109)
    * Add Contact Page
    
    * Add Contact Page
    
    * Fix MUI bug, remove unused imports
    
    * Remove console.logs
    
    * Fix searchfield bug, add dropdown up-down navigation

---
+++ UX/frontend/src/utils/data/contact.ts
@@ -0,0 +1,35 @@
+export const timeSlots = [
+  '08:00-09:00',
+  '09:00-10:00',
+  '10:00-11:00',
+  '11:00-12:00',
+  '13:00-14:00',
+  '14:00-15:00',
+  '15:00-16:00',
+  '16:00-17:00',
+];
+
+export const INITIAL_FORM_DATA = {
+  firstName: '',
+  surName: '',
+  companyName: '',
+  email: '',
+  telephone: {
+    code: 'SE',
+    label: 'Sweden',
+    phone: '46',
+    number: '',
+  },
+  businessField: 'Select an option',
+  message: '',
+  booking: null,
+};
+
+export const INITIAL_FORM_VALIDITY = {
+    firstName: '',
+    surName: '',
+    companyName: '',
+    email: '',
+    businessField: '',
+    message: '',
+  }
