99bc77eae406d0a3b389678118191c05e0e7c6f2
Emil Kronholm <emil.kronholm@gmail.com>
2025-05-15 18:39:43 +0200

    Fix recomp

--- Application/app/src/main/java/se/terrax9/ui/screens/controller/LiveStream.kt
+++ Application/app/src/main/java/se/terrax9/ui/screens/controller/LiveStream.kt
@@ -4,18 +4,21 @@ import android.annotation.SuppressLint
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCompositionContext
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -47,29 +50,27 @@ fun VideoStream(
             modifier = modifier
                 .fillMaxSize()
         ) {
-            AndroidView(factory = { context ->
-                WebView(context).apply {
-                    settings.apply {
-                        javaScriptEnabled = true
-                        mediaPlaybackRequiresUserGesture = false
-                        setSupportZoom(true)
-                        builtInZoomControls = true
-                        displayZoomControls = false
-                        loadWithOverviewMode = true
-                        useWideViewPort = true
+            key (hej) {
+                AndroidView(
+                    factory = { context ->
+                        WebView(context).apply {
+                            settings.javaScriptEnabled = true
+                            webChromeClient = WebChromeClient()
+                            loadUrl("file:///android_asset/stream_player.html?key=$hej")
+                        }
                     }
-                    webChromeClient = WebChromeClient()
-                    loadUrl("file:///android_asset/stream_player.html")
-                }
-            })
+                )
+            }
 
             // This layer captures clicks above the WebView
             Box(
                 modifier = Modifier
                     .matchParentSize()
-                    .clickable {
-                        println("click")
-                        hej++
+                    .pointerInput(Unit) {
+                        detectTapGestures {
+                            println("click")
+                            hej++
+                        }
                     }
             )
 
@@ -77,7 +78,7 @@ fun VideoStream(
             Text(
                 text = "$hej",
                 color = Color.Transparent,
-                fontSize = 0.sp
+                fontSize = 30.sp
             )
         }
 
